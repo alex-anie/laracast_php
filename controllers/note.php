d3baf31d78998bd2cb7890a236e5c369478302a9
alex-anie <ocxigin@gmail.com>
2025-05-17 15:10:14 +0100

    Programming is Rewritting

--- controllers/note.php
+++ controllers/note.php
@@ -5,9 +5,21 @@
     $db = new Database($config['database']);
 
     $heading = 'Note';
+    $currentUserId = 1;
 
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])-> fetch();
 
-    // dd($notes);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+        ])-> fetch();
+
+    // dd($note);
+
+    if(! $note){
+        abort();
+    }
+
+    if($note['user_id'] !== $currentUserId){
+        abort(Response::FORBIDDEN);
+    }
 
     require "views/note.view.php";
