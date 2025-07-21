e55c280eb5d4893a55b175a8d2d0e52de93471ff
alex-anie <ocxigin@gmail.com>
2025-07-21 18:40:10 +0100

    hash password

--- project_two/controllers/registration/store.php
+++ project_two/controllers/registration/store.php
@@ -30,13 +30,13 @@ $user = $db->query('SELECT * FROM `users` WHERE `email` = :email', [
 ])->find();
 
 if($user){
-    
+
     header('location: /');
     exit();
 }else {
     $db->query('INSERT INTO `users`(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     // mark that the user has logged in
